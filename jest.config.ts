a96562c399d63906b013b1ba39513c13d70675e7
Simon Heather <simon.heather@yulife.com>
2026-05-05 17:42:36 +0100

    Update nx affected command and fix linting and knip

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,8 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
-import type { Config } from 'jest';
-import { getJestProjectsAsync } from '@nx/jest';
+import type { Config } from "jest";
+import { getJestProjectsAsync } from "@nx/jest";
 
 export default async (): Promise<Config> => ({
   projects: await getJestProjectsAsync(),
